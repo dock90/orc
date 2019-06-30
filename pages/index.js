fa0cea84a2bada22b91eb0d83e835c3db0ec2c1c
EdwardGoomba <edward@dock90.io>
2019-06-30 12:12:42 -0400

    Add netlify deploy script

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ import Button from '@material-ui/core/Button';
 const Index = () => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
-      <title>Index page</title>
-      <meta name="description" content="This is a description of the Index page" />
+      <title>ORC</title>
+      <meta name="description" content="Online Runway Calculator is a tool for solo creative business owners who want to run a profitable business." />
     </Head>
     <p>Content on Index page</p>
     <Button variant="contained">MUI button</Button>
